17d5ae3a7b8a8d8cf5614384554535d949edaf69
Stefano Sarina <stefano.sarina@zucchetti.com>
2023-12-19 17:32:04 +0100

    .

--- C37797W/XmlTest/XmlTest/Program.cs
+++ C37797W/XmlTest/XmlTest/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace XmlTest
+﻿using System.Xml;
+
+namespace XmlTest;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        //(?i)(_\d{1,2}){1,4}(_){0,1}
+        //var xmlDocument = new System.Xml.XmlDocument();
+        //xmlDocument.Load("E:\\Documenti\\Dati\\Develop\\Sts.Software\\Work.Stuff\\C37797W\\Data\\report.xml");
+        //xmlDocument["NewDataSet"]["Header"].ChildNodes.OfType<XmlNode>();
     }
 }
